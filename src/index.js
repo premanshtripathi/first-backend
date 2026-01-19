7e52eb8dfc2b50107b2fce0d46c98954e044402f
Premansh Tripathi <98167095+premanshtripathi@users.noreply.github.com>
2026-01-20 02:09:27 +0530

    add utilities

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./.env",
 });
-connectDB();
+const PORT = process.env.PORT || 8000;
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("❌ App is not Listening !! Error: ", error);
+      throw error;
+    });
+    app.listen(PORT, () => {
+      console.log(`✅ Server is running at port: ${PORT}`);
+      console.log(`✅ Process PID: ${process.pid}`);
+    });
+  })
+  .catch((error) => {
+    console.log("❌ MongoDB connection failed: ", error);
+  });
 
 /* import express from "express";
 const app = express();
