8e78c8264a71778e6531ed76eed59394a4e7538f
Premansh Tripathi <98167095+premanshtripathi@users.noreply.github.com>
2026-02-18 23:44:54 +0530

    add validation for required fields and to prevent self subscription

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -5,11 +5,18 @@ const subscriptionSchema = new Schema(
     subscriber: {
       type: Schema.Types.ObjectId, // the one who is subscribing.
       ref: "User",
+      required: [true, "Subscriber Id is required!"],
     },
 
     channel: {
       type: Schema.Types.ObjectId, // the one to whom subscriber is subscribing.
       ref: "User",
+      required: [true, "Channel Id is required!"],
+      validate: {
+        validator: function (channelId) {
+          return channelId.toString() !== this.subscriber.toString();
+        },
+      },
     },
   },
   { timestamps: true }
