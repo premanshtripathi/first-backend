fce443a9b11b533362617218a748cc067abfc2e1
Premansh Tripathi <98167095+premanshtripathi@users.noreply.github.com>
2026-02-20 23:50:11 +0530

    implement getUserChannelSubscribers subscribers

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -45,6 +45,59 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+  const { page = 1, limit = 50 } = req.query;
+  const userId = req.user?._id;
+
+  if (!isValidObjectId(channelId) || !isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid channel or user Id!");
+  }
+
+  if (channelId !== userId.toString()) {
+    throw new ApiError(
+      401,
+      "You are not authorized to view subscribers of this channel!"
+    );
+  }
+
+  const pipeline = [];
+
+  pipeline.push({
+    $match: { channel: new mongoose.Types.ObjectId(channelId) },
+  });
+
+  pipeline.push({
+    $lookup: {
+      from: "users",
+      localField: "subscriber",
+      foreignField: "_id",
+      as: "subscriberDetails",
+      pipeline: [{ $project: { username: 1, fullname: 1, avatar: 1 } }],
+    },
+  });
+
+  pipeline.push({ $unwind: "$subscriberDetails" });
+
+  const options = {
+    page: parseInt(page),
+    limit: Math.min(100, parseInt(limit)),
+    customLabels: {
+      docs: "subscribers",
+      totalDocs: "totalSubscribers",
+    },
+  };
+
+  const result = await Subscription.aggregatePaginate(
+    Subscription.aggregate(pipeline),
+    options
+  );
+
+  if (!result) {
+    throw new ApiError(500, "Something went wrong while getting subscribers!");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, result, "subscribers fetched successfully."));
 });
 
 // controller to return channel list to which user has subscribed
